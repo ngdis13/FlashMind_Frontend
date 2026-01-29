e8be15388daeb2b26cbcee4ef09b068964afea30
ngdis13 <the.n_kocherovaaa@bk.ru>
2026-01-29 19:08:03 +0300

    Добавила файлы для общих стилей и цветов

---
+++ styles/Colors.tsx
@@ -0,0 +1,11 @@
+export const colors = {
+  background: '#F6F9FC',
+
+  mainColor: '#6E75D9',
+  darkMainColor: '282B54',
+
+  lightGray: 'E3E3E3',
+  darkGray: '777777',
+
+  darkColor: '000000',
+};
