ff0e8570d436a079ec83d6a5b9f91a9d85c61f39
Анастасия <the.n_kocherovaaa@bk.ru>
2026-05-01 21:30:21 +0300

    test3

---
+++ app/decks/[id]/_layout.tsx
@@ -0,0 +1,5 @@
+import { Stack } from 'expo-router';
+
+export default function DecksIdLayout() {
+  return <Stack screenOptions={{ headerShown: false }}></Stack>;
+}
--- app/decks/_layout.tsx
+++
@@ -1,5 +0,0 @@
-import { Stack } from 'expo-router';
-
-export default function DecksLayout() {
-  return <Stack screenOptions={{ headerShown: false }}></Stack>;
-}
