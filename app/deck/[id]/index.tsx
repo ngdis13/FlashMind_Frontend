1cf0c1e66bc76ab62c478a459a098706a315a8f6
Анастасия <the.n_kocherovaaa@bk.ru>
2026-05-01 20:42:15 +0300

    test19

--- app/deck/[id]/index.tsx
+++ app/deck/[id]/index.tsx
@@ -1,113 +1,112 @@
-// import { commonStyles } from "@/styles/Common";
-// import { Typography } from "@/styles/Typography";
-// import { useFocusEffect, useLocalSearchParams, useRouter } from "expo-router";
+import { commonStyles } from "@/styles/Common";
+import { Typography } from "@/styles/Typography";
+import { useFocusEffect, useLocalSearchParams, useRouter } from "expo-router";
 import { ScrollView, View, Image, Pressable, FlatList } from "react-native";
-// import ReturnIcon from "@/assets/icons/ReturnIcon.png";
-// import { styles } from "../styles/deckViewById.style";
-// import { Input } from "@/components/Input";
-// import { useCallback, useEffect, useState } from "react";
-// import { SettingsIcon } from "@/feature/profile/assets/SettingsIcon";
-// import PlusIcon from "@/assets/icons/PlusIcon.png";
-// import searchButton from "@/feature/decks/assets/searchButton.png";
-// import { colors } from "@/styles/Colors";
-// import { Logo } from "@/components/Logo";
-// import { useDecks } from "@/storage/hooks/useDecks";
-// import { CardItem } from "../components/CardItem";
-// import { Card } from "@/storage/types/types";
+import ReturnIcon from "@/assets/icons/ReturnIcon.png";
+import { styles } from "../styles/deckViewById.style";
+import { Input } from "@/components/Input";
+import { useCallback, useEffect, useState } from "react";
+import { SettingsIcon } from "@/feature/profile/assets/SettingsIcon";
+import PlusIcon from "@/assets/icons/PlusIcon.png";
+import searchButton from "@/feature/decks/assets/searchButton.png";
+import { colors } from "@/styles/Colors";
+import { Logo } from "@/components/Logo";
+import { useDecks } from "@/storage/hooks/useDecks";
+import { CardItem } from "../components/CardItem";
+import { Card } from "@/storage/types/types";
 
 export default function DeckViewById() {
-  // const { id } = useLocalSearchParams<{ id: string }>();
-  // const router = useRouter();
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const router = useRouter();
 
-  // const { decks, loading, getDeckCards, removeCard } = useDecks();
+  const { decks, loading, getDeckCards, removeCard } = useDecks();
 
-  // const [name, setName] = useState("");
-  // const [description, setDescription] = useState("");
-  // const [search, setSearch] = useState("");
-  // const [cards, setCards] = useState<Card[]>([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [search, setSearch] = useState("");
+  const [cards, setCards] = useState<Card[]>([]);
 
-  // const deck = decks.find((d) => d.id === id);
+  const deck = decks.find((d) => d.id === id);
 
-  // const handleBack = () => {
-  //   router.back();
-  // };
-  // const handleSettings = () => {
-  //   //Переход в настройки колоды
-  // };
-  // const handleAddCard = () => {
-  //   //Создание новой карточки в колоде
-  //   router.push(`/deck/${id}/create-card`);
-  // };
-  // const startSearch = () => {
-  //   //поиск по карточкам
-  // };
-  // const loadCards = async () => {
-  //   try {
-  //     const fetchedCards = await getDeckCards(id as string);
-  //     setCards(fetchedCards);
-  //     console.log("карточки колоды: ", fetchedCards);
-  //   } catch (error) {
-  //     console.error("Ошибка загрузки карточек:", error);
-  //   }
-  // };
-  // const handleCardPress = (cardId: string) => {
-  //   router.push(`/card/${cardId}?deckId=${id}`);
-  // };
+  const handleBack = () => {
+    router.back();
+  };
+  const handleSettings = () => {
+    //Переход в настройки колоды
+  };
+  const handleAddCard = () => {
+    //Создание новой карточки в колоде
+    router.push(`/deck/${id}/create-card`);
+  };
+  const startSearch = () => {
+    //поиск по карточкам
+  };
+  const loadCards = async () => {
+    try {
+      const fetchedCards = await getDeckCards(id as string);
+      setCards(fetchedCards);
+      console.log("карточки колоды: ", fetchedCards);
+    } catch (error) {
+      console.error("Ошибка загрузки карточек:", error);
+    }
+  };
+  const handleCardPress = (cardId: string) => {
+    router.push(`/card/${cardId}?deckId=${id}`);
+  };
 
-  // const handleDeleteCard = async (cardId: string, deckId?: string) => {
-  //   try {
-  //     await removeCard(deckId || (id as string), cardId);
-  //     setCards((prevCards) => prevCards.filter((card) => card.id !== cardId));
-  //     console.log("Карточка удалена");
-  //   } catch (err) {
-  //     console.error("Ошибка при удалении карточки:", err);
-  //   }
-  // };
+  const handleDeleteCard = async (cardId: string, deckId?: string) => {
+    try {
+      await removeCard(deckId || (id as string), cardId);
+      setCards((prevCards) => prevCards.filter((card) => card.id !== cardId));
+      console.log("Карточка удалена");
+    } catch (err) {
+      console.error("Ошибка при удалении карточки:", err);
+    }
+  };
 
-  // // Рендер отдельной карточки
-  // const renderCard = ({ item, index }: { item: Card; index: number }) => (
-  //   <CardItem
-  //     id={item.id}
-  //     front={item.front}
-  //     back={item.back}
-  //     deckId={id}
-  //     index={index}
-  //     viewMode="compact"
-  //     onPress={handleCardPress}
-  //     onDelete={handleDeleteCard}
-  //   />
-  // );
+  // Рендер отдельной карточки
+  const renderCard = ({ item, index }: { item: Card; index: number }) => (
+    <CardItem
+      id={item.id}
+      front={item.front}
+      back={item.back}
+      deckId={id}
+      index={index}
+      viewMode="compact"
+      onPress={handleCardPress}
+      onDelete={handleDeleteCard}
+    />
+  );
 
-  // // Загружаем карточки колоды
-  // useEffect(() => {
-  //   if (id) {
-  //     loadCards();
-  //   }
-  // }, [id]);
+  // Загружаем карточки колоды
+  useEffect(() => {
+    if (id) {
+      loadCards();
+    }
+  }, [id]);
 
-  // // Заполняем форму данными из колоды
-  // useEffect(() => {
-  //   if (deck) {
-  //     setName(deck.name);
-  //     setDescription(deck.description || "");
-  //   }
-  // }, [deck]);
+  // Заполняем форму данными из колоды
+  useEffect(() => {
+    if (deck) {
+      setName(deck.name);
+      setDescription(deck.description || "");
+    }
+  }, [deck]);
 
-  // useFocusEffect(
-  //   useCallback(() => {
-  //     // Обновляем карточки когда возвращаемся на экран
-  //     if (id) {
-  //       loadCards();
-  //     }
-  //   }, [id]),
-  // );
+  useFocusEffect(
+    useCallback(() => {
+      // Обновляем карточки когда возвращаемся на экран
+      if (id) {
+        loadCards();
+      }
+    }, [id]),
+  );
 
-  // const hasCards = cards.length > 0;
+  const hasCards = cards.length > 0;
 
   return (
-    <View style={[ { flex: 1 }]}>
-      {/* <View style={[commonStyles.container, { flex: 1 }]}></View> */}
-      {/* <FlatList
+    <View style={[commonStyles.container, { flex: 1 }]}>
+      <FlatList
         data={cards}
         keyExtractor={(item) => item.id}
         renderItem={renderCard}
@@ -180,7 +179,7 @@ export default function DeckViewById() {
           ) : null
         }
         contentContainerStyle={{ paddingHorizontal: 10, paddingBottom: 30 }}
-      /> */}
+      />
     </View>
   );
 }
